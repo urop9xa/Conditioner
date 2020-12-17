2152b12e6769be4b0598065253cccb923161706b
urop9xa <urop9xa@gmail.com>
2020-12-17 17:38:45 +0300

    Add test 100% branch

--- src/test/java/ru/netology/ConditionerTest.java
+++ src/test/java/ru/netology/ConditionerTest.java
@@ -1,4 +1,3 @@
-
 package ru.netology;
 
 import org.junit.jupiter.api.Test;
@@ -7,13 +6,98 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ConditionerTest {
     @Test
-    public void shouldGetAndSet() {
-        Conditioner conditioner = new Conditioner();
-        String expected = "Кондишн";
+    public void SetCurrentTemperatureValid() {
+        Conditioner condition = new Conditioner();
+        condition.setName("Panasonic");
+        condition.setMaxTemperature(25);
+        condition.setMinTemperature(17);
+        condition.setOn(true);
+        condition.setCurrentTemperature(20);
+        int actual = condition.getCurrentTemperature();
+        int expected = 20;
+        assertEquals(expected, actual);
 
-        assertNull(conditioner.getName());
-        conditioner.setName(expected);
-        assertEquals(expected, conditioner.getName());
     }
 
+    @Test
+    public void SetCurrentTemperatureInvalidMax() {
+        Conditioner condition = new Conditioner();
+        condition.setName("Panasonic");
+        condition.setMaxTemperature(25);
+        condition.setMinTemperature(17);
+        condition.setOn(true);
+        condition.setCurrentTemperature(30);
+        int actual = condition.getCurrentTemperature();
+        int expected = 0;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void SetCurrentTemperatureInvalidMin() {
+        Conditioner condition = new Conditioner();
+        condition.setName("Panasonic");
+        condition.setMaxTemperature(25);
+        condition.setMinTemperature(17);
+        condition.setOn(true);
+        condition.setCurrentTemperature(15);
+        int actual = condition.getCurrentTemperature();
+        int expected = 0;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void increaseCurrentTemperatureValid() {
+        Conditioner condition = new Conditioner();
+        condition.setName("Panasonic");
+        condition.setMaxTemperature(25);
+        condition.setMinTemperature(17);
+        condition.setOn(true);
+        condition.setCurrentTemperature(20);
+        condition.increaseCurrentTemperature();
+        assertEquals(21, condition.getCurrentTemperature());
+
+
+    }
+
+    @Test
+    public void increaseCurrentTemperatureInvalid() {
+        Conditioner condition = new Conditioner();
+        condition.setName("Panasonic");
+        condition.setMaxTemperature(25);
+        condition.setMinTemperature(17);
+        condition.setOn(true);
+        condition.setCurrentTemperature(25);
+        condition.increaseCurrentTemperature();
+        assertEquals(25, condition.getCurrentTemperature());
+
+
+    }
+
+    @Test
+    public void decreaseCurrentTemperatureValid() {
+        Conditioner condition = new Conditioner();
+        condition.setName("Panasonic");
+        condition.setMaxTemperature(25);
+        condition.setMinTemperature(17);
+        condition.setOn(true);
+        condition.setCurrentTemperature(19);
+        condition.decreaseCurrentTemperature();
+        assertEquals(18, condition.getCurrentTemperature());
+
+    }
+
+    @Test
+    public void decreaseCurrentTemperatureInvalid() {
+        Conditioner condition = new Conditioner();
+        condition.setName("Panasonic");
+        condition.setMaxTemperature(25);
+        condition.setMinTemperature(17);
+        condition.setOn(true);
+        condition.setCurrentTemperature(17);
+        condition.decreaseCurrentTemperature();
+        assertEquals(17, condition.getCurrentTemperature());
+
+    }
 }
